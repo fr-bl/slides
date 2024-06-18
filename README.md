d9f0b8b786cb67fb617424fd4211a8b28bc4d64d
frederick <git@blaesing.me>
2024-06-18 13:43:15 +0200

    init slides

---
+++ README.md
@@ -0,0 +1,6 @@
+# Slides
+
+[![Slides öffnen](https://custom-icon-badges.demolab.com/badge/Slides_öffnen-121212?style=for-the-badge&logo=globe&logoColor=ce93d8)](https://fr-bl.github.io/slides)
+[![Mit Astro erstellt](https://custom-icon-badges.demolab.com/badge/Mit_Astro_erstellt-121212?style=for-the-badge&logo=astro&logoColor=#BC52EE)](https://astro.build)
+
+Meine eigenen Präsentationen.
---
+++ astro.config.ts
@@ -0,0 +1,25 @@
+import { defineConfig } from "astro/config";
+import icon from "astro-icon";
+import favicons from "astro-favicons";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [
+    icon({
+      include: {},
+    }),
+    favicons({
+      appName: "Fredericks Präsentationen",
+      appShortName: "Präsis",
+      appDescription: "Meine eigenen Präsentationen.",
+      path: "/",
+      masterPicture: "./src/assets/favicon.svg",
+    }),
+  ],
+  site: "https://fr-bl.github.io/slides",
+  vite: {
+    ssr: {
+      noExternal: ["@fontsource-variable/montserrat"],
+    },
+  },
+});
